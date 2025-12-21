ffebe36a3465c93f03f9f687f605b71d6d418573
Muhammad Usama <muhammadusama3228@gmail.com>
2025-12-21 14:49:00 +0500

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     serverComponentsExternalPackages: [],
   },
   images: {
     unoptimized: false,
   },
-  // Remove output: 'standalone' for Vercel deployment
-  // output: 'standalone',
 }
 
 export default nextConfig
